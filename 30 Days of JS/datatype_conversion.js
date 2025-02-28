ee67be7f0b1f0670224e2b00641935e408b9673c
Tanushree Borase <tanushreebcs@gmail.com>
2025-02-28 12:12:20 +0000

    conversion

---
+++ 30 Days of JS/datatype_conversion.js
@@ -0,0 +1,36 @@
+let cricscore = "100tan";
+let score = 0;
+let name = "Archana";
+
+console.log(typeof cricscore)
+console.log(typeof(cricscore)) //method
+
+let valueInNumber = Number(cricscore)
+console.log(typeof valueInNumber)  // number
+console.log(valueInNumber) // NaN
+
+let valueinnumber = Number(score);
+console.log(valueinnumber) // 0
+
+let valueInnumber = Number(name);
+console.log(valueInnumber) // NaN
+
+// "33" => 33
+//  true => 1; false => 0
+
+let isLoggedIn = "Tanushree"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // true
+
+let someNumber = 27
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); // string
+
+
+
+
+
+
