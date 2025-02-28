f3c603e67fcd11f4ed3e244f9f45f78ebc61241f
Tanushree Borase <tanushreebcs@gmail.com>
2025-02-28 10:48:51 +0000

    datatypes

---
+++ 30 Days of JS/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+alert("hello")
+
+alert(3+3) // error occurs in terminal
+
+let name = "Tanushree"
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
