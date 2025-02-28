81bbac246e94cf2089bdbde558daa0d009bacf37
Tanushree Borase <tanushreebcs@gmail.com>
2025-02-28 23:27:42 +0530

    Update stringconversion.js

--- 30 Days of JS/stringconversion.js
+++ 30 Days of JS/stringconversion.js
@@ -21,10 +21,10 @@ let str2 = " Tanushree "
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);  12
+// console.log(1 + "2"); 12
+// console.log("1" + 2 + 2); 122
+// console.log(1 + 2 + "2"); 32
 
 // console.log( (3 + 4) * 5 % 3);
 
