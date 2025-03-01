a1b3a971a251e350996ae154f0f38ecffe49c6f8
Tanushree Borase <tanushreebcs@gmail.com>
2025-03-01 16:07:42 +0530

    Create Stack and Heap Memory.js

---
+++ 30 Days of JS/Stack and Heap Memory.js
@@ -0,0 +1,36 @@
+// Stack(Primitive) Heap(Non primitive)
+
+let myYTname = "codegyan";
+
+let anotherYTname = myYTname
+anotherYTname = "ccode"
+
+console.log(myYTname) // codegyan
+console.log(anotherYTname) // ccode
+
+/*
+Stack Memory (Primitive Data Types)
+Used for storing primitive values (e.g., number, string, boolean, null, undefined, symbol, and bigint).
+Operates on a Last In, First Out (LIFO) basis.
+*/
+
+
+let userOne = {
+  email: samajcode@gmail.com;
+  upi: 0009@hgy;
+}
+
+let userTwo = userOne;
+
+let userTwo = {
+  userTwo.email = codemast@gmail.com;
+}
+
+console.log(userOne.email); // codemast@gmail.com
+console.log(userTwo.email); // codemast@gmail.com
+
+/*
+Heap Memory (Reference Data Types)
+Used for objects, arrays, and functions.
+Allocated dynamically and accessed via references.
+*/
