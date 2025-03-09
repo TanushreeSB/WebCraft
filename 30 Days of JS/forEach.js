da3c93d87ff90e9033640ac23028832d6caf7049
Tanushree Borase <tanushreebcs@gmail.com>
2025-03-09 14:58:21 +0530

    Update forEach.js

--- 30 Days of JS/forEach.js
+++ 30 Days of JS/forEach.js
@@ -1,22 +1,40 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
+// The forEach() method of Array instances executes a provided function once for each array element.
 // coding.forEach( function (val){
 //     console.log(val);
 // } )
 
+/* output:
+js
+ruby
+java
+python
+cpp
+*/
+
+
 // coding.forEach( (item) => {
-//     console.log(item);
+//     console.log(item); /// same output as above
 // } )
 
 // function printMe(item){
-//     console.log(item);
+//     console.log(item); // same output as above
 // }
 
-// coding.forEach(printMe)
+// coding.forEach(printMe) // giving reference to function
 
 // coding.forEach( (item, index, arr)=> {
-//     console.log(item, index, arr);
+//     console.log(item, index, arr); // 
 // } )
+/*
+output:
+js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+*/
 
 const myCoding = [
     {
@@ -34,6 +52,11 @@ const myCoding = [
 ]
 
 myCoding.forEach( (item) => {
-    
     console.log(item.languageName);
 } )
+/*
+    output:
+    javascript
+    java
+    python
+*/
